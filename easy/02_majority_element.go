c87435163062d9dd2374b5ce5ebaae30aae4dd15
Artem Lavrinovich <art.lavrinovich@outlook.com>
2022-05-16 22:28:23 +0300

    Fixed majority element solution

--- easy/02_majority_element.go
+++ easy/02_majority_element.go
@@ -3,23 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	test := []int{2, 2, 2, 2, 3, 3, 2, 3, 2, 5, 3, 3}
+	test := []int{3, 2, 3}
 	fmt.Print(getMajority(test))
 }
 
 func getMajority(list []int) int {
-	var res = list[0]
-	var count = 1
+	var res = 0
+	var count = 0
 
-	for i := 1; i < len(list); i += 1 {
+	for i := 0; i < len(list); i += 1 {
+		if count == 0 {
+			res = list[i]
+		}
 		if res == list[i] {
 			count += 1
 		} else {
 			count -= 1
-		}
-
-		if count < 0 {
-			res = list[i]
 		}
 	}
 
